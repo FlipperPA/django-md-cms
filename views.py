47c739987ec4217881ce3af68028933d28e67233
Allen, Timothy <tallen@wharton.upenn.edu>
2015-05-25 07:07:32 -0700

    Test whether f.read or StringIO is faster.

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.views.generic.base import TemplateView
-from markdown import markdownFromFile
+
+from markdown import markdown
+
 
 class MdCMSView(TemplateView):
     """ Render a Markdown file as HTML within a Django template. """
@@ -8,11 +10,10 @@ class MdCMSView(TemplateView):
     template_name = "md_cms/index.html"
 
     def get_context_data(self, **kwargs):
-#        print(**kwargs)
         context = super(MdCMSView, self).get_context_data(**kwargs)
-        s = StringIO()
-        markdownFromFile(input='/vagrant/html/markdown_cms/md_cms/README.md', output=s)
-        context['markdown_text'] = s.getvalue()
-        s.close()
+
+        f = open('/vagrant/html/markdown_cms/md_cms/README.md', 'r')
+        context['markdown_text'] = markdown(f.read())
+        f.close()
 
         return context
