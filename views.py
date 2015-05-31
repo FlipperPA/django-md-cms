71aba9b07b5e9e469d4a820ed21c1514e5af946a
Allen, Timothy <tallen@wharton.upenn.edu>
2015-05-31 08:20:32 -0700

    Install MD_CMS_ROOT setting.

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.views.generic.base import TemplateView
 
 from markdown import markdown
+
 
 class MdCMSView(TemplateView):
     """ Render a Markdown file as HTML within a Django template. """
@@ -11,7 +13,7 @@ class MdCMSView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MdCMSView, self).get_context_data(**kwargs)
 
-        with open('/vagrant/html/markdown_cms/md_cms/README.md', 'r') as f:
+        with open(settings.MD_CMS_ROOT + 'README.md', 'r') as f:
             context['markdown_text'] = markdown(f.read())
 
         return context
