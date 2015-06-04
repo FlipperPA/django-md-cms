80627be46a7ffa7942e23f13f1dbe93891b51374
Allen, Timothy <tallen@wharton.upenn.edu>
2015-06-04 07:21:31 -0700

    Fix up default form for one PageDown textarea.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
-from django import Forms
+from django import forms
 from pagedown.widgets import PagedownWidget
 
-class MdCMSForm(Ffrms.form):
+class MdCMSForm(forms.Form):
     md_cms_form = forms.CharField(widget=PagedownWidget())
