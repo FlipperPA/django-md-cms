9068760399c01fc95535891460a8b4c62e9ed483
Allen, Timothy <tallen@wharton.upenn.edu>
2015-06-11 10:39:31 -0700

    Start package for install.

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-md-cms',
+    version='0.1',
+    packages=['md_cms'],
+    include_package_data=True,
+    license='BSD License',
+    description='A simple Django CMS which reads markdown files from the filesystem and provides the Pagedown editor.',
+    long_description=README,
+    url='http://www.example.com/',
+    author='Timothy Allen',
+    author_email='tim.allen@peregrinesalon.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
