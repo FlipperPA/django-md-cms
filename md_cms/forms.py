8e56c0f8c7862c0a18d4e6066a34ea8a64cec2b7
Allen, Timothy <tallen@wharton.upenn.edu>
2015-06-21 09:49:11 -0700

    Test out javascript markdown rendering for tests.

--- md_cms/forms.py
+++ md_cms/forms.py
@@ -5,5 +5,5 @@ from pagedown.widgets import PagedownWidget
 class MdCMSForm(forms.Form):
     md_cms_form = forms.CharField(
         widget=PagedownWidget(),
-        initial='ASDF ASDF ASDF',
+        initial='# Insert initial content here\n\n**Testing**\n\n    class CodeBlockTest():\n        def __init__():\n            pass\n',
     )
