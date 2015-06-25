84be3e46d9e5145e0e8c13f72014a5945d73d3fa
Allen, Timothy <tallen@wharton.upenn.edu>
2015-06-25 11:24:47 -0700

    Include required packages.

--- setup.py
+++ setup.py
@@ -14,9 +14,14 @@ setup(
     license='BSD License',
     description='A simple Django CMS which reads markdown files from the filesystem based on the Django route and provides the Pagedown editor.',
     long_description=README,
+    long_description=open('README.md', encoding='utf-8').read(),
     url='https://github.com/FlipperPA/django-md-cms',
     author='Timothy Allen',
-    author_email='tim.allen@peregrinesalon.com',
+    author_email='tim@pyphilly.org',
+    install_required=[
+        'django-pagedown==0.1.0',
+        'Markdown==2.6.2',
+    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -29,5 +34,6 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
